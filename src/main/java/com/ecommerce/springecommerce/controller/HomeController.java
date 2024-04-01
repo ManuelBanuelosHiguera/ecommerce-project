f15f7c7f1aa07b4479d8673d744aed079ec207c0
testusuario <manuel_bob182@hotmail.com>
2024-04-01 17:50:50 -0600

    add home allows see the main page in localhost, add HomeController endpoint to root of localhost

---
+++ src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.springecommerce.controller;
+
+import com.ecommerce.springecommerce.repository.ProductoRepository;
+import com.ecommerce.springecommerce.service.IProductoService;
+import com.ecommerce.springecommerce.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("")
+    public String home(Model model){
+        model.addAttribute("productos", productoService.findAll());
+        return "usuario/home";
+    }
+
+}
