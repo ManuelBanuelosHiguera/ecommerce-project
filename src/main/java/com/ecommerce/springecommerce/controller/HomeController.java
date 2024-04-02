8653b725e434e5b04fdad696b89ba5ad9be11d32
testusuario <manuel_bob182@hotmail.com>
2024-04-01 20:34:54 -0600

    add view ver producto, add method to redirect productohome in home controller, add productohome in template/usuario

--- src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
@@ -3,16 +3,20 @@ package com.ecommerce.springecommerce.controller;
 import com.ecommerce.springecommerce.repository.ProductoRepository;
 import com.ecommerce.springecommerce.service.IProductoService;
 import com.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private IProductoService productoService;
@@ -21,6 +25,11 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        log.info("Id enviado como parametro {}", id);
+        return "usuario/productohome";
     }
 
 }
