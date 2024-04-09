fac6c35f39b181b5394b8d4811fe604ab851c705
testusuario <manuel_bob182@hotmail.com>
2024-04-09 10:13:30 -0600

    add view product by unit and return home from the product view

--- src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.springecommerce.controller;
 
+import com.ecommerce.springecommerce.model.Producto;
 import com.ecommerce.springecommerce.repository.ProductoRepository;
 import com.ecommerce.springecommerce.service.IProductoService;
 import com.ecommerce.springecommerce.service.ProductoService;
@@ -12,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -27,8 +30,14 @@ public class HomeController {
         return "usuario/home";
     }
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model){
         log.info("Id enviado como parametro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional=productoService.get(id);
+        producto=productoOptional.get();
+
+        model.addAttribute("producto",producto);
+
         return "usuario/productohome";
     }
 
