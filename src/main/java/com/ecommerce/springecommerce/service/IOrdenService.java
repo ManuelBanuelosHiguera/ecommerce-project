336c9a1319f16bc6fc0b7afc5bf5554f52b46f6b
testusuario <manuel_bob182@hotmail.com>
2024-04-17 13:18:21 -0600

    method to generate order number

--- src/main/java/com/ecommerce/springecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/springecommerce/service/IOrdenService.java
@@ -4,9 +4,11 @@ import com.ecommerce.springecommerce.model.Orden;
 import org.springframework.stereotype.Service;
 
 import javax.print.attribute.standard.OutputDeviceAssigned;
+import java.util.List;
 
 @Service
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save (Orden orden);
 }
 
